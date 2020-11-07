5b6932424453f9f72e4a21cd487afdfef473d206
tejas bhuwania <tejasbhuwania2805@gmail.com>
2020-11-07 18:37:09 +0800

    update reqs

--- picture_click.py
+++ picture_click.py
@@ -1,14 +1,12 @@
 import cv2
 
-
-
 def click_save_picture(img_num):
 
     cap = cv2.VideoCapture(0) # video capture source camera (Here webcam of laptop)
     ret,frame = cap.read() # return a single frame in variable `frame`
-    
+
     while True:
-        
+
         #print("IN CAMERA WHOOPIEEE: ")
         file_name = 'img' + str(img_num) + '.png'
         cv2.imshow('img1',frame) #display the captured image
