43bc9e9639197edbe1a59d516f637b9bbc0d0110
rwizz122 <79588374+rwizz122@users.noreply.github.com>
2021-02-27 19:28:22 +0800

    Update RestaurantPage.js

--- src/components/RestaurantPage.js
+++ src/components/RestaurantPage.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import Alert from 'react-bootstrap/Alert';
+  
+import React from 'react';
 
-function Example() {
-  return (
-    <Alert dismissible variant="danger">
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>
-        Change this and that and try again.
-      </p>
-    </Alert>
-  )
+class RestaurantPage extends React.Component{
+  render() {
+    return (
+        <div>
+        <h2> About </h2>
+        <p> This app uses React</p>
+    </div>
+  );
+  }
 }
+
+export default RestaurantPage
