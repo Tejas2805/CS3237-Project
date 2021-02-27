e0cb331d72bea453536f16424dc9fcd790de3f3b
rwizz122 <79588374+rwizz122@users.noreply.github.com>
2021-02-27 17:58:14 +0800

    Update HomePage.js

--- hello-world-app/src/components/HomePage.js
+++ hello-world-app/src/components/HomePage.js
@@ -5,10 +5,10 @@ function HomePage() {
   return (
       <div className = "jumbotron">
         <h1> WELCOME TO OUR WEBSITE </h1>
-        <p> Click to select your user profile
-            RESTAURANT
-            VOLUNTEER
-            BUYERS</p>
+        <p> Click to select your user profile </p>
+        <p> <Link to="about" className="btn btn-primary">RESTAURANT</Link></p>
+        <p><Link to="about" className="btn btn-primary">VOLUNTEER</Link></p>
+        <p><Link to="about" className="btn btn-primary">BUYER/CUSTOMER</Link></p>
         <Link to="about" className="btn btn-primary">About</Link>
       </div>
   );
