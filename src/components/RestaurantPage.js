a8a41f7d6377ef798dab546d485682959bf1f916
rwizz122 <79588374+rwizz122@users.noreply.github.com>
2021-02-27 21:19:15 +0800

    Update RestaurantPage.js

--- src/components/RestaurantPage.js
+++ src/components/RestaurantPage.js
@@ -1,15 +1,39 @@
-  
-import React from 'react';
+import React, {useState} from 'react';
+import Header from "./common/Header";
+import BuyerForm from "./common/BuyerForm";
 
-class RestaurantPage extends React.Component{
-  render() {
-    return (
-        <div>
-        <h2> About </h2>
-        <p> This app uses React</p>
+const BuyerPage = props => {
+
+  const [packet, setPacket] = useState({
+    wt: "",
+    numOfPacket: ""
+  });
+
+  function handleTitleChange(event) {
+    const updatedPacket = {...packet, wt:event.target.value};
+    setPacket(updatedPacket);
+  }
+
+  function handleNmChange(event) {
+    const updatedPacket = {...packet, numOfPacket:event.target.value};
+    setPacket(updatedPacket);
+  }
+
+  function handleChange(event) {
+    const updatedPacket = {...packet, [event.target.name]:event.target.value};
+    setPacket(updatedPacket);
+  }
+
+  function handleSubmit(event) {
+    alert('An essay was submitted: ');
+    event.preventDefault();
+  }
+
+  return (
+      <div>
+      <BuyerForm packet={packet} onTitleChange={handleTitleChange} onNmChange={handleNmChange} />
     </div>
   );
-  }
 }
 
-export default RestaurantPage
+export default BuyerPage
