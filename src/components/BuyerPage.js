c1a2d512e00a832402d8eec157936a1ab02ef536
rwizz122 <79588374+rwizz122@users.noreply.github.com>
2021-02-27 19:42:57 +0800

    Update BuyerPage.js

--- src/components/BuyerPage.js
+++ src/components/BuyerPage.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import Alert from 'react-bootstrap/Alert'
-
-<Alert variant="success">
-  <Alert.Heading>Welcome, User!</Alert.Heading>
-  <p>
-    Looks like you successfully read this important alert message!
-  </p>
-  <hr />
-  <p className="mb-0">
-    This is a margin where you place other things. Like links and other goodies for the user. It breaks things up well. 
-  </p>
-</Alert>
-
 class BuyerPage extends React.Component{
   render() {
     return (
