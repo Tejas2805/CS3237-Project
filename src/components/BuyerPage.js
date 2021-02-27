ff1f62fd855c1ecc9d80ab4dac61839afd6db9f7
tejas bhuwania <tejasbhuwania2805@gmail.com>
2021-02-27 18:32:52 +0800

    update files

---
+++ src/components/BuyerPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BuyerPage extends React.Component{
+  render() {
+    return (
+        <div>
+        <h2> About </h2>
+        <p> This app uses React</p>
+    </div>
+  );
+  }
+}
+
+export default BuyerPage
---
+++ src/components/VolunteerPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class VolunteerPage extends React.Component{
+  render() {
+    return (
+        <div>
+        <h2> About </h2>
+        <p> This app uses React</p>
+    </div>
+  );
+  }
+}
+
+export default VolunteerPage
